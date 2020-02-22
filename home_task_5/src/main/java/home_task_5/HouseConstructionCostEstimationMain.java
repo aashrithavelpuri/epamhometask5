501c69e7d62ef0e9bf93b0c7216b3761fb5fc8de
aashrithavelpuri <60144824+aashrithavelpuri@users.noreply.github.com>
2020-02-22 22:28:53 +0530

    Add files via upload

---
+++ home_task_5/src/main/java/home_task_5/HouseConstructionCostEstimationMain.java
@@ -0,0 +1,12 @@
+package home_task_5;
+
+import home_task_5.HouseConstructionCostEstimation;
+
+public class HouseConstructionCostEstimationMain
+{
+	public static void main(String args[])
+	{
+		HouseConstructionCostEstimation estimate = new HouseConstructionCostEstimation();
+		estimate.costPerSquareFeet();
+	}
+}
